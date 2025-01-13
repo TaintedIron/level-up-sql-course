be638debbe15d9d54489f0d6c0139c3c3e3b5e79
David <59463977+TaintedIron@users.noreply.github.com>
2025-01-13 20:04:18 +0000

    Answer 01_06

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,14 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+SELECT
+  *
+FROM
+  Customers
+WHERE FirstName = 'Norby';
+
+SELECT * FROM Reservations WHERE customerID = 64;
+
+DELETE FROM Reservations WHERE customerID = 64 AND ReservationID = 2000;
+
+SELECT * FROM Reservations WHERE customerID = 64 ORDER BY Date DESC;
