50703f843d6ae5cc21ea5ec8f81ec431f6257515
David <59463977+TaintedIron@users.noreply.github.com>
2025-01-13 19:55:14 +0000

    Answer 01_05
    When `UPDATE` remember to follow table_name with `SET` to update record. Had to peak at solution for `SET`.

--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -2,3 +2,20 @@
 
 -- Taylor Jenkins, of 27170 6th Ave., Washington, DC,
 -- has moved to 74 Pine St., New York, NY.
+SELECT
+  *
+FROM
+  Customers
+WHERE
+  FirstName = 'Taylor'
+  AND LastName = 'Jenkins';
+
+UPDATE Customers 
+SET 
+  Address = '74 Pine St.',
+  City = 'New York',
+  State = 'NY'
+WHERE
+  customerID = 26;
+
+SELECT * FROM Customers WHERE customerID = 26;
