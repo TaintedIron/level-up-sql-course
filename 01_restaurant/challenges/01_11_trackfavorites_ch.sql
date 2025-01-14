67b70bcafd6078766dddb36c9d33330afbabcc42
David <59463977+TaintedIron@users.noreply.github.com>
2025-01-14 14:59:12 +0000

    answer 01_11

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,11 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+SELECT * FROM Customers WHERE LastName = 'Goldwater';
+SELECT * FROM Dishes WHERE Name = 'Quinoa Salmon Salad';
+
+UPDATE Customers SET FavoriteDish = (SELECT DishID FROM Dishes WHERE Name = 'Quinoa Salmon Salad')
+WHERE LastName = 'Goldwater' and FirstName = 'Cleo';
+
+SELECT * FROM Customers WHERE LastName = 'Goldwater';
