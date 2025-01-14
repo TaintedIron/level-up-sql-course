68ef332575007effe545c32bef7765e2a51b9c62
David <59463977+TaintedIron@users.noreply.github.com>
2025-01-14 19:45:00 +0000

    Last one: Answer for 02_08

--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -5,3 +5,28 @@
 
 -- Report 2: Show the five books that have been
 -- checked out the most.
+
+-- Report 1: Titles Published by Year
+SELECT
+  Published,
+  COUNT(DISTINCT(Title)) AS PubCount
+FROM 
+  Books
+GROUP BY 
+  Published
+ORDER BY
+  PubCount DESC;
+
+-- Report 2: Top Five Books by Loan Count
+SELECT
+  Books.Title,
+  COUNT(Loans.LoanID) AS LoanCount
+FROM
+  Loans
+  JOIN Books ON Loans.BookID = Books.BookID
+GROUP BY
+  Books.Title
+ORDER BY
+  LoanCount DESC
+LIMIT
+  5;
