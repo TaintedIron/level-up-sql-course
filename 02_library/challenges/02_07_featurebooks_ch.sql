ce53f91d6d2453b83574d1f61c847fb23497de16
David <59463977+TaintedIron@users.noreply.github.com>
2025-01-14 19:38:41 +0000

    Answer 02_07.
    little tricky with the nested query following `WHERE` and `AND`.

--- 02_library/challenges/02_07_featurebooks_ch.sql
+++ 02_library/challenges/02_07_featurebooks_ch.sql
@@ -2,3 +2,22 @@
 
 -- Make a pick list of books published from 1890-1899 
 -- which are not currently checked out.
+
+SELECT
+  Books.Title,
+  Books.Author,
+  Books.Published,
+  Books.Barcode
+FROM
+  Books
+WHERE
+  Books.Published BETWEEN 1890 AND 1899
+  AND (Books.BookID NOT IN 
+    (SELECT
+      Loans.BookID
+    FROM
+      Loans
+    WHERE
+      Loans.ReturnedDate IS NULL))
+ORDER BY 
+  Books.Title;
