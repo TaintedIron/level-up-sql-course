e86fe08b94a4aba7b24b043b4d63423f73e2584b
David <59463977+TaintedIron@users.noreply.github.com>
2025-01-14 18:46:18 +0000

    answer 02_03

--- 02_library/challenges/02_03_checkout_ch.sql
+++ 02_library/challenges/02_03_checkout_ch.sql
@@ -3,3 +3,41 @@
 -- Book 2: Great Expectations, 4043822646
 -- The checkout date is August 25, 2022 and the 
 -- due date is September 8, 2022.
+SELECT PatronID, LastName, FirstName, Email 
+    FROM Patrons 
+    WHERE Email = 'jvaan@wisdompets.com';
+
+SELECT * 
+    FROM Loans
+    WHERE PatronID = 50
+    ORDER BY LoanDate DESC;
+
+-- Book 1 Checkout
+INSERT INTO Loans (BookID, PatronID, LoanDate, DueDate)
+VALUES (
+  (SELECT BookID 
+    FROM Books 
+    WHERE Barcode = '2855934983'),
+  (SELECT PatronID 
+    FROM Patrons 
+    WHERE Email = 'jvaan@wisdompets.com'),
+  '2022-08-25',
+  '2022-09-08'
+  );
+
+-- Book 2 Checkout
+INSERT INTO Loans (BookID, PatronID, LoanDate, DueDate)
+VALUES (
+  (SELECT BookID 
+    FROM Books 
+    WHERE Barcode = '4043822646'),
+  (SELECT PatronID 
+    FROM Patrons 
+    WHERE Email = 'jvaan@wisdompets.com'),
+  '2022-08-25',
+  '2022-09-08'
+  );
+
+-- forgot '' in book1 checkout. delete loanid 2001 in loans table
+-- DELETE FROM Loans
+-- WHERE LoanID = 2001;
